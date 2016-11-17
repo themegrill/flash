e6161fccc38be90b575706bc9aa416f6af42efa3
rabindratharu <rabindra.tharu.np@gmail.com>
2016-11-17 15:57:58 +0545

    added rest of demo data files.

--- inc/demo-config.php
+++ inc/demo-config.php
@@ -25,7 +25,6 @@ function tg_demo_importer_config( $demo_config ) {
 		'flash-default' => array(
 			'name'         => __( 'Flash Default', 'flash' ),
 			'demo_url'     => 'http://demo.themegrill.com/flash/',
-			'demo-pack'    => false,
 			'core_options' => array(
 				'blogname'       => 'Flash',
 				'page_on_front'  => 'Home',
@@ -48,19 +47,19 @@ function tg_demo_importer_config( $demo_config ) {
 					 */
 					'dropdown_categories' => array(
 						'category' => array (
-							'themegrill_flash_blog' => array(
-							   18 => array(
-								  'category' => 'Blog'
-							   )
+							'FT_Widget_Blog' => array(
+								14 => array(
+									'category' => 'Blog'
+								)
 							),
 						),
 						'portfolio_cat' => array(
-							'themegrill_flash_portfolio' => array(
-								5 => array(
+							'FT_Widget_Portfolio' => array(
+								4 => array(
 									'categories' => 'portfolio'
 								)
 							),
-						)
+						),
 					  )
 				   )
 				)
@@ -74,7 +73,218 @@ function tg_demo_importer_config( $demo_config ) {
 			'plugins_list' => array(
 				'required' => array(
 					'siteorigin-panels' => array(
-						'name' => __( 'Page Builder', 'flash' ),
+						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
+						'slug' => 'siteorigin-panels/siteorigin-panels.php',
+						'link' => 'https://wordpress.org/plugins/siteorigin-panels/'
+					),
+					'flash-toolkit' => array(
+						'name' => __( 'Flash Toolkit', 'flash' ),
+						'slug' => 'flash-toolkit/flash-toolkit.php',
+						'link' => 'https://wordpress.org/plugins/flash-toolkit/'
+					),
+				),
+				'recommended' => array(
+					'contact-form-7' => array(
+						'name' => __( 'Contact Form', 'flash' ),
+						'slug' => 'contact-form-7/contact-form-7.php',
+						'link' => 'https://wordpress.org/plugins/contact-form-7/'
+					),
+				)
+			)
+		),
+		'flash-corporate' => array(
+			'name'         => __( 'Flash Corporate', 'flash' ),
+			'demo_url'     => 'http://demo.themegrill.com/flash-corporate/',
+			'core_options' => array(
+				'blogname'       => 'Flash Corporate',
+				'page_on_front'  => 'Home',
+				'page_for_posts' => 'Blog',
+			),
+			'siteorigin_panels_data_update' => array(
+				'homepage' => array(
+				   'post_title'  => 'Home',
+				   'data_update' => array(
+
+					/**
+					 * Dropdown Categories - Handles widgets Category ID.
+					 *
+					 * A. Core Post Category:
+					 *    1. themegrill_flash_portfolio
+					 *    2. themegrill_flash_blog
+					 *
+					 * Note: Supported Taxonomy:
+					 *    A. Core Post Category - category
+					 */
+					'dropdown_categories' => array(
+						'category' => array (
+							'FT_Widget_Blog' => array(
+								6 => array(
+									'category' => 'building'
+								)
+							),
+						),
+						'portfolio_cat' => array(
+							'FT_Widget_Portfolio' => array(
+								4 => array(
+									'categories' => 'portfolio'
+								)
+							),
+						),
+					  )
+				   )
+				)
+			),
+			'customizer_data_update' => array(
+				'nav_menu_locations' => array(
+					'primary' => 'Primary Menu',
+				)
+			),
+			'plugins_list' => array(
+				'required' => array(
+					'siteorigin-panels' => array(
+						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
+						'slug' => 'siteorigin-panels/siteorigin-panels.php',
+						'link' => 'https://wordpress.org/plugins/siteorigin-panels/'
+					),
+					'flash-toolkit' => array(
+						'name' => __( 'Flash Toolkit', 'flash' ),
+						'slug' => 'flash-toolkit/flash-toolkit.php',
+						'link' => 'https://wordpress.org/plugins/flash-toolkit/'
+					),
+				),
+				'recommended' => array(
+					'contact-form-7' => array(
+						'name' => __( 'Contact Form', 'flash' ),
+						'slug' => 'contact-form-7/contact-form-7.php',
+						'link' => 'https://wordpress.org/plugins/contact-form-7/'
+					),
+				)
+			)
+		),
+		'flash-food' => array(
+			'name'         => __( 'Flash Food', 'flash' ),
+			'demo_url'     => 'http://demo.themegrill.com/flash-food/',
+			'core_options' => array(
+				'blogname'       => 'Flash Food',
+				'page_on_front'  => 'Home',
+				'page_for_posts' => 'Blog',
+			),
+			'siteorigin_panels_data_update' => array(
+				'homepage' => array(
+				   'post_title'  => 'Home',
+				   'data_update' => array(
+
+					/**
+					 * Dropdown Categories - Handles widgets Category ID.
+					 *
+					 * A. Core Post Category:
+					 *    1. themegrill_flash_portfolio
+					 *    2. themegrill_flash_blog
+					 *
+					 * Note: Supported Taxonomy:
+					 *    A. Core Post Category - category
+					 */
+					'dropdown_categories' => array(
+						'category' => array (
+							'FT_Widget_Blog' => array(
+								5 => array(
+									'category' => 'Blog'
+								)
+							),
+						),
+						'portfolio_cat' => array(
+							'FT_Widget_Portfolio' => array(
+								4 => array(
+									'categories' => 'portfolio'
+								)
+							),
+						),
+					  )
+				   )
+				)
+			),
+			'customizer_data_update' => array(
+				'nav_menu_locations' => array(
+					'primary'	=> 'Primary Menu',
+					'social'	=> 'Social',
+					'footer'	=> 'Footer Menu',
+				)
+			),
+			'plugins_list' => array(
+				'required' => array(
+					'siteorigin-panels' => array(
+						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
+						'slug' => 'siteorigin-panels/siteorigin-panels.php',
+						'link' => 'https://wordpress.org/plugins/siteorigin-panels/'
+					),
+					'flash-toolkit' => array(
+						'name' => __( 'Flash Toolkit', 'flash' ),
+						'slug' => 'flash-toolkit/flash-toolkit.php',
+						'link' => 'https://wordpress.org/plugins/flash-toolkit/'
+					),
+				),
+				'recommended' => array(
+					'contact-form-7' => array(
+						'name' => __( 'Contact Form', 'flash' ),
+						'slug' => 'contact-form-7/contact-form-7.php',
+						'link' => 'https://wordpress.org/plugins/contact-form-7/'
+					),
+				)
+			)
+		),
+		'flash-construction' => array(
+			'name'         => __( 'Flash Construction', 'flash' ),
+			'demo_url'     => 'http://demo.themegrill.com/flash-construction/',
+			'core_options' => array(
+				'blogname'       => 'Flash Construction',
+				'page_on_front'  => 'Home',
+				'page_for_posts' => 'Blog',
+			),
+			'siteorigin_panels_data_update' => array(
+				'homepage' => array(
+				   'post_title'  => 'Home',
+				   'data_update' => array(
+
+					/**
+					 * Dropdown Categories - Handles widgets Category ID.
+					 *
+					 * A. Core Post Category:
+					 *    1. themegrill_flash_portfolio
+					 *    2. themegrill_flash_blog
+					 *
+					 * Note: Supported Taxonomy:
+					 *    A. Core Post Category - category
+					 */
+					'dropdown_categories' => array(
+						'category' => array (
+							'FT_Widget_Blog' => array(
+								9 => array(
+									'category' => 'Blog'
+								)
+							),
+						),
+						'portfolio_cat' => array(
+							'FT_Widget_Portfolio' => array(
+								4 => array(
+									'categories' => 'portfolio'
+								)
+							),
+						),
+					  )
+				   )
+				)
+			),
+			'customizer_data_update' => array(
+				'nav_menu_locations' => array(
+					'primary'	=> 'Main',
+					'social'	=> 'Social',
+					'footer'	=> 'Footer Menu',
+				)
+			),
+			'plugins_list' => array(
+				'required' => array(
+					'siteorigin-panels' => array(
+						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
 						'slug' => 'siteorigin-panels/siteorigin-panels.php',
 						'link' => 'https://wordpress.org/plugins/siteorigin-panels/'
 					),
