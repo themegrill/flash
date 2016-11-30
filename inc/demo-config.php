a040a428f3d2d99f7acf6fd48711b4f66f12e4f8
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-11-30 17:03:49 +0545

    Demo packs config

--- inc/demo-config.php
+++ inc/demo-config.php
@@ -12,153 +12,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_filter( 'themegrill_demo_importer_config', 'flash_demo_importer_config' );
-
-/**
- * Setup demo importer config.
- *
- * @param  array $demo_config
- * @return array
- */
-function flash_demo_importer_config( $demo_config ) {
-	$new_demo_config = array(
+function flash_demo_importer_installable_packs( $demo_packs ) {
+	$new_demo_packs = array(
+		'flash-default' => array(
+			'name'    => __( 'Flash Default', 'flash' ),
+			'preview' => 'http://demo.themegrill.com/flash/',
+		),
 		'flash-onepage' => array(
-			'name'         => __( 'Flash OnePage', 'flash' ),
-			'demo_url'     => 'http://demo.themegrill.com/flash-one-page/',
-			'demo_pack'    => true,
-			'core_options' => array(
-				'blogname'       => 'Flash One Page',
-				'page_on_front'  => 'Home',
-				'page_for_posts' => 'Blog',
-			),
-			'siteorigin_panels_data_update' => array(
-				'homepage' => array(
-					'post_title'  => 'Home',
-					'data_update' => array(
-
-						/**
-						 * Dropdown Categories - Handles widgets Category ID.
-						 *
-						 * A. Core Post Category:
-						 *    1. themegrill_flash_portfolio
-						 *    2. themegrill_flash_blog
-						 *
-						 * Note: Supported Taxonomy:
-						 *    A. Core Post Category - category
-						 */
-						'dropdown_categories' => array(
-							'portfolio_cat' => array(
-								'FT_Widget_Portfolio' => array(
-									4 => array(
-										'categories' => 'portfolio'
-									)
-								),
-							),
-							'category' => array (
-								'FT_Widget_Blog' => array(
-									8 => array(
-										'category' => 'building'
-									)
-								),
-							),
-						)
-					)
-				)
-			),
-			'customizer_data_update' => array(
-				'nav_menu_locations' => array(
-					'primary' => 'Primary Menu',
-				)
-			),
-			'plugins_list' => array(
-				'required' => array(
-					'siteorigin-panels' => array(
-						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
-						'slug' => 'siteorigin-panels/siteorigin-panels.php',
-					),
-					'flash-toolkit' => array(
-						'name' => __( 'Flash Toolkit', 'flash' ),
-						'slug' => 'flash-toolkit/flash-toolkit.php',
-					),
-				),
-				'recommended' => array(
-					'contact-form-7' => array(
-						'name' => __( 'Contact Form', 'flash' ),
-						'slug' => 'contact-form-7/contact-form-7.php',
-					),
-				)
-			)
+			'name'    => __( 'Flash OnePage', 'flash' ),
+			'preview' => 'http://demo.themegrill.com/flash-one-page/',
 		),
 		'flash-food' => array(
-			'name'         => __( 'Flash Food', 'flash' ),
-			'demo_url'     => 'http://demo.themegrill.com/flash-food/',
-			'demo_pack'    => true,
-			'core_options' => array(
-				'blogname'       => 'Flash Food',
-				'page_on_front'  => 'Home',
-				'page_for_posts' => 'Blog',
-			),
-			'siteorigin_panels_data_update' => array(
-				'homepage' => array(
-					'post_title'  => 'Home',
-					'data_update' => array(
-
-						/**
-						 * Dropdown Categories - Handles widgets Category ID.
-						 *
-						 * A. Core Post Category:
-						 *    1. themegrill_flash_portfolio
-						 *    2. themegrill_flash_blog
-						 *
-						 * Note: Supported Taxonomy:
-						 *    A. Core Post Category - category
-						 */
-						'dropdown_categories' => array(
-							'portfolio_cat' => array(
-								'FT_Widget_Portfolio' => array(
-									5 => array(
-										'categories' => 'All category'
-									)
-								),
-							),
-							'category' => array (
-								'FT_Widget_Blog' => array(
-									8 => array(
-										'category' => 'Blog'
-									)
-								),
-							),
-						)
-					)
-				)
-			),
-			'customizer_data_update' => array(
-				'nav_menu_locations' => array(
-					'primary'	=> 'Primary',
-					'social'	=> 'Social',
-					'footer'	=> 'Footer Menu',
-				)
-			),
-			'plugins_list' => array(
-				'required' => array(
-					'siteorigin-panels' => array(
-						'name' => __( 'Page Builder by SiteOrigin', 'flash' ),
-						'slug' => 'siteorigin-panels/siteorigin-panels.php',
-					),
-					'flash-toolkit' => array(
-						'name' => __( 'Flash Toolkit', 'flash' ),
-						'slug' => 'flash-toolkit/flash-toolkit.php',
-					),
-				),
-				'recommended' => array(
-					'contact-form-7' => array(
-						'name' => __( 'Contact Form', 'flash' ),
-						'slug' => 'contact-form-7/contact-form-7.php',
-					),
-				)
-			)
-		)
+			'name'    => __( 'Flash Food', 'flash' ),
+			'preview' => 'http://demo.themegrill.com/flash-food/',
+		),
+		'flash-construction' => array(
+			'name'    => __( 'Flash Construction', 'flash' ),
+			'preview' => 'http://demo.themegrill.com/flash-construction/',
+		),
 	);
 
-	return array_merge( $new_demo_config, $demo_config );
+	return array_merge( $new_demo_packs, $demo_packs );
 }
+add_filter( 'themegrill_demo_importer_installable_packs', 'flash_demo_importer_installable_packs' );
