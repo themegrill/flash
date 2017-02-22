3753637f17d684cbdf87257479125bfa76eb4d36
RajeebTheGreat <rajeebthegreat@gmail.com>
2017-02-22 16:23:28 +0545

    Fix - jQuery One Page Nav
    Link: https://github.com/mmiszy/jQuery-One-Page-Nav

--- js/flash.js
+++ js/flash.js
@@ -48,15 +48,6 @@ jQuery(document).ready(function() {
 		    scrollThreshold: 0.5,
 		    filter: '',
 		    easing: 'swing',
-		    begin: function() {
-		        //I get fired when the animation is starting
-		    },
-		    end: function() {
-		        //I get fired when the animation is ending
-		    },
-		    scrollChange: function() {
-		        //I get fired when you enter a section and I pass the list item of the section
-		    }
 		});
 	});
 
