9bd0bcda7186d318d7717d542a711f218a07a34e
saazaan7 <sajanjoshi0@gmail.com>
2017-04-20 16:10:24 +0545

    Fix - Check if the page is woocommerce only if woocommerce is active

--- inc/extras.php
+++ inc/extras.php
@@ -515,7 +515,6 @@ function flash_custom_fonts() {
 		<?php
 	}
 }
-
 /**
 * Returns true if on a page which uses WooCommerce templates (cart and checkout are standard pages with shortcodes and which are also included)
 *
@@ -523,25 +522,32 @@ function flash_custom_fonts() {
 * @return bool
 */
 function flash_is_woocommerce_page () {
-	if(  function_exists ( 'is_woocommerce' ) && is_woocommerce()){
+
+	if(  flash_is_woocommerce_active() ) {
+		
+		if( is_woocommerce() ) {
 			return true;
-	}
-	$woocommerce_keys   =   array ( "woocommerce_shop_page_id" ,
-									"woocommerce_terms_page_id" ,
-									"woocommerce_cart_page_id" ,
-									"woocommerce_checkout_page_id" ,
-									"woocommerce_pay_page_id" ,
-									"woocommerce_thanks_page_id" ,
-									"woocommerce_myaccount_page_id" ,
-									"woocommerce_edit_address_page_id" ,
-									"woocommerce_view_order_page_id" ,
-									"woocommerce_change_password_page_id" ,
-									"woocommerce_logout_page_id" ,
-									"woocommerce_lost_password_page_id" ) ;
-	foreach ( $woocommerce_keys as $wc_page_id ) {
+		}
+
+		$woocommerce_keys   =   array ( "woocommerce_shop_page_id" ,
+										"woocommerce_terms_page_id" ,
+										"woocommerce_cart_page_id" ,
+										"woocommerce_checkout_page_id" ,
+										"woocommerce_pay_page_id" ,
+										"woocommerce_thanks_page_id" ,
+										"woocommerce_myaccount_page_id" ,
+										"woocommerce_edit_address_page_id" ,
+										"woocommerce_view_order_page_id" ,
+										"woocommerce_change_password_page_id" ,
+										"woocommerce_logout_page_id" ,
+										"woocommerce_lost_password_page_id" ) ;
+
+		foreach ( $woocommerce_keys as $wc_page_id ) {
 			if ( get_the_ID () == get_option ( $wc_page_id , 0 ) ) {
-					return true ;
+					return true;
 			}
+		}
+
 	}
 	return false;
 }
