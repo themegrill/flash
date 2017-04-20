94c13a52c831fc68a550bf847e49441f90656403
RajeebTheGreat <rajeebthegreat@gmail.com>
2017-04-20 15:56:43 +0545

    Fix - flash_is_woocommerce_page function

--- inc/extras.php
+++ inc/extras.php
@@ -516,6 +516,36 @@ function flash_custom_fonts() {
 	}
 }
 
+/**
+* Returns true if on a page which uses WooCommerce templates (cart and checkout are standard pages with shortcodes and which are also included)
+*
+* @access public
+* @return bool
+*/
+function flash_is_woocommerce_page () {
+	if(  function_exists ( 'is_woocommerce' ) && is_woocommerce()){
+			return true;
+	}
+	$woocommerce_keys   =   array ( "woocommerce_shop_page_id" ,
+									"woocommerce_terms_page_id" ,
+									"woocommerce_cart_page_id" ,
+									"woocommerce_checkout_page_id" ,
+									"woocommerce_pay_page_id" ,
+									"woocommerce_thanks_page_id" ,
+									"woocommerce_myaccount_page_id" ,
+									"woocommerce_edit_address_page_id" ,
+									"woocommerce_view_order_page_id" ,
+									"woocommerce_change_password_page_id" ,
+									"woocommerce_logout_page_id" ,
+									"woocommerce_lost_password_page_id" ) ;
+	foreach ( $woocommerce_keys as $wc_page_id ) {
+			if ( get_the_ID () == get_option ( $wc_page_id , 0 ) ) {
+					return true ;
+			}
+	}
+	return false;
+}
+
 add_action( 'tgmpa_register', 'flash_register_required_plugins' );
 /**
  * Register the required plugins for this theme.
