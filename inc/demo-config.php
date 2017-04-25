7990d60721f12bac9fc3e9b43af2d37c7e837192
Bishal Napit <mebishalnapit@gmail.com>
2017-04-25 11:22:36 +0545

    Add the theme demo support for Flash Pro default

--- inc/demo-config.php
+++ inc/demo-config.php
@@ -22,27 +22,32 @@ function flash_demo_importer_packages( $packages ) {
 	$new_packages = array(
 		'flash-default' => array(
 			'name'    => __( 'Flash Default', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash/',
+			'preview' => 'https://demo.themegrill.com/flash/',
 		),
 		'flash-onepage' => array(
 			'name'    => __( 'Flash OnePage', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash-one-page/',
+			'preview' => 'https://demo.themegrill.com/flash-one-page/',
 		),
 		'flash-food' => array(
 			'name'    => __( 'Flash Food', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash-food/',
+			'preview' => 'https://demo.themegrill.com/flash-food/',
 		),
 		'flash-construction' => array(
 			'name'    => __( 'Flash Construction', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash-construction/',
+			'preview' => 'https://demo.themegrill.com/flash-construction/',
 		),
 		'flash-food-v2' => array(
 			'name'    => __( 'Flash Food V2', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash-food-v2/',
+			'preview' => 'https://demo.themegrill.com/flash-food-v2/',
 		),
 		'flash-construction-v2' => array(
 			'name'    => __( 'Flash Construction V2', 'flash' ),
-			'preview' => 'http://demo.themegrill.com/flash-construction-v2/',
+			'preview' => 'https://demo.themegrill.com/flash-construction-v2/',
+		),
+		'flash-pro-default' => array(
+			'name'    => __( 'Flash Pro', 'flash' ),
+			'preview' => 'https://demo.themegrill.com/flash-pro/',
+			'pro_link'=> 'https://themegrill.com/themes/flash/'
 		),
 	);
 
