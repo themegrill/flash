af1eb43700fb8618e0370edc18651eedb685b467
Sanjeev Shrestha <sethstha@gmail.com>
2017-09-06 11:56:41 +0545

    Fix - menu not showing if closed on mobile menu

--- js/flash.js
+++ js/flash.js
@@ -94,7 +94,7 @@ jQuery(document).ready(function() {
 	jQuery(window).load(function() {
 	
 		jQuery('.main-navigation .menu-toggle').click(function() {
-			jQuery('.main-navigation .menu').slideToggle('slow');
+			jQuery('.main-navigation .menu').toggleClass('show-menu');
 		});
 	
 		if(typeof Swiper === 'function'){
