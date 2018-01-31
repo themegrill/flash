86d05233fbc5964aea1ac08781c47f075a3c17a4
Ashish Shrestha <ashishsthanp@gmail.com>
2018-01-31 17:22:17 +0545

    Fix - header text live js

--- js/customizer.js
+++ js/customizer.js
@@ -6,39 +6,56 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-( function( $ ) {
+( function ( $ ) {
 	var style = $( '#flash-color-scheme-css' ),
 		api = wp.customize;
 
 	if ( ! style.length ) {
 		style = $( 'head' ).append( '<style type="text/css" id="flash-color-scheme-css" />' )
-		                    .find( '#flash-color-scheme-css' );
+			.find( '#flash-color-scheme-css' );
 	}
 
 	// Site title.
-	api( 'blogname', function( value ) {
-		value.bind( function( to ) {
+	api( 'blogname', function ( value ) {
+		value.bind( function ( to ) {
 			$( '.site-title a' ).text( to );
 		} );
 	} );
 
 	// Site tagline.
-	api( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
+	api( 'blogdescription', function ( value ) {
+		value.bind( function ( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
 
+	// Header text color.
+	api( 'header_textcolor', function ( value ) {
+		value.bind( function ( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title, .site-description' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title, .site-description' ).css( {
+					'clip': 'auto',
+					'position': 'relative'
+				} );
+			}
+		} );
+	} );
+
 	// Add custom-background-image body class when background image is added.
-	api( 'background_image', function( value ) {
-		value.bind( function( to ) {
+	api( 'background_image', function ( value ) {
+		value.bind( function ( to ) {
 			$( 'body' ).toggleClass( 'custom-background-image', '' !== to );
 		} );
 	} );
 
 	// Color Scheme CSS.
-	api.bind( 'preview-ready', function() {
-		api.preview.bind( 'update-color-scheme-css', function( css ) {
+	api.bind( 'preview-ready', function () {
+		api.preview.bind( 'update-color-scheme-css', function ( css ) {
 			style.html( css );
 		} );
 	} );
