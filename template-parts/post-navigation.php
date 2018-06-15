4b655a137989b8c971e2009b56989584db3e74be
Prabha Lama <lprabha47@gmail.com>
2018-06-15 14:46:57 +0545

    Navigation arrow change on RTL

--- template-parts/post-navigation.php
+++ template-parts/post-navigation.php
@@ -4,9 +4,18 @@
  *
  * @package Flash
  */
+
+if (is_rtl()) {
+	$next = "fa-angle-left";
+	$prev = "fa-angle-right";
+} else {
+	$next = "fa-angle-right";
+	$prev = "fa-angle-left";
+}
+
 the_post_navigation( array(
-	'next_text' => '<span><i class="fa fa-angle-right"></i></span>
+	'next_text' => '<span><i class="fa ' . $next . '"></i></span>
 		<span class="entry-title">%title</span>',
-	'prev_text' => '<span><i class="fa fa-angle-left"></i></span>
+	'prev_text' => '<span><i class="fa ' . $prev . '"></i></span>
 		<span class="entry-title">%title</span>',
 ) );
