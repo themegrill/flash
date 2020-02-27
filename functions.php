3c6ee03f38b223900444d1e00d6548b3121ec681
Ashish <ashishsthanp@gmail.com>
2020-02-27 11:02:38 +0545

    Remove - Unnecessary code

--- functions.php
+++ functions.php
@@ -287,9 +287,9 @@ require get_template_directory() . '/inc/siteorigin-panels.php';
  */
 if ( is_admin() ) {
 	require get_template_directory() . '/inc/class-flash-admin.php';
-	require get_template_directory() . '/inc/class-flash-site-library.php';
 	require get_template_directory() . '/inc/admin/class-flash-theme-review-notice.php';
 	require get_template_directory() . '/inc/admin/class-flash-tdi-notice.php';
+	require get_template_directory() . '/inc/admin/class-flash-dashboard.php';
 }
 
 /**
