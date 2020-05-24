9fb53f127d61afeee5c1ee17bc1276661048c75b
ThemeGrill <milanthapa299@gmail.com>
2020-05-24 11:38:15 +0545

    Added required CSS and enqueues CSS, JS files and renamed file

--- inc/admin/class-flash-admin.php
+++ inc/admin/class-flash-admin.php
@@ -22,8 +22,25 @@ if ( ! class_exists( 'Flash_Admin' ) ) :
 		 * Constructor.
 		 */
 		public function __construct() {
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		}
 
+		/**
+		 * Localize array for import button AJAX request.
+		 */
+		public function enqueue_scripts() {
+			wp_enqueue_style( 'flash-admin-style', get_template_directory_uri() . '/inc/admin/css/admin.css', array(), FLASH_THEME_VERSION );
+
+			wp_enqueue_script( 'flash-plugin-install-helper', get_template_directory_uri() . '/inc/admin/js/plugin-handle.js', array( 'jquery' ), FLASH_THEME_VERSION, true );
+
+			$welcome_data = array(
+				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&flash-hide-notice=welcome' ) ),
+				'btn_text' => esc_html__( 'Processing...', 'flash' ),
+				'nonce'    => wp_create_nonce( 'flash_demo_import_nonce' ),
+			);
+
+			wp_localize_script( 'flash-plugin-install-helper', 'flashRedirectDemoPage', $welcome_data );
+		}
 	}
 
 endif;
