c92a65f4af29340840d25182a3ca6f704ab2af31
Premiii <jimeepremi@gmail.com>
2021-05-11 09:52:42 +0545

    Update - Ugrade notice more discount

--- inc/admin/class-flash-upgrade-notice.php
+++ inc/admin/class-flash-upgrade-notice.php
@@ -78,8 +78,8 @@ class Flash_Upgrade_Notice extends Flash_Notice {
 					),
 					'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
 					'Flash',
-					'<code class="coupon-code">upgrade15</code>',
-					'15%'
+					'<code class="coupon-code">TGFREEUSER</code>',
+					'25%'
 				);
 				?>
 			</p>
